59b96733d321d28c4129604115485d6406f9c3a0
Tarannum <84217141+tarannum-2002@users.noreply.github.com>
2023-01-24 12:58:01 +0530

    Update TestMacro.c

--- Week-11/TestMacro.c
+++ Week-11/TestMacro.c
@@ -3,70 +3,37 @@
 //configuration
 //options supported on any given system using feature test macros.
 
-
 #define _POSIX_SOURCE
-
-#define _POSIX_C_SOURCE199309L
-
+#define _POSIX_C_SOURCE 199309L
 #include<stdio.h>
-
 #include<unistd.h>
 
-int main()
-
-{
-
+int main(){
 #ifdef _POSIX_JOB_CONTROL
-
 printf("System supports job control\n");
-
 #else
-
-printf("System doesnot support job control \n");
-
+printf("System does not support job control \n");
 #endif
-
 #ifdef _POSIX_SAVED_IDS
-
-printf("Systemsupports saved set-UID and saved set-GID\n");
-
+printf("System supports saved set-UID and saved set-GID\n");
 #else
-
-printf("System doesnot support saved set-UID and saved set-GID \n");
-
+printf("System does not support saved set-UID and saved set-GID \n");
 #endif
-
 #ifdef _POSIX_CHOWN_RESTRICTED
-
-printf("chown_restrictedoption is %d\n”,_POSIX_CHOWN_RESTRICTED);
-
+printf("chown_restricted option is %d\n",_POSIX_CHOWN_RESTRICTED);
 #else
-
-printf("System doesnot support chown_restricted option \n");
-
+printf("System does not support chown_restricted option \n");
 #endif
-
 #ifdef _POSIX_NO_TRUNC
-
 printf("Pathname trunc option is %d\n",_POSIX_NO_TRUNC);
-
 #else
-
-printf("System doesnot support system-wide pathname trunc option \n");
-
+printf("System does not support system-wide pathname trunc option \n");
 #endif
-
 #ifdef _POSIX_VDISABLE
-
-printf("Disablecharacter for terminal files is %d\n",_POSIX_VDISABLE);
-
+printf("Disable character for terminal files is %d\n", _POSIX_VDISABLE);
 #else
-
-printf(" System doesnot support _POSIX_VDISABLE \n");
-
+printf(" System does not support _POSIX_VDISABLE \n");
 #endif
-
 return 0;
-
 }
 
